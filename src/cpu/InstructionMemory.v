075e1501bf0008296b4f8ccef0bb54effa8c048e
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-01-09 15:34:50 +0100

    changed the code

--- src/cpu/InstructionMemory.v
+++ src/cpu/InstructionMemory.v
@@ -22,16 +22,20 @@
 
 module InstructionMemory(
 input wire[23:0] PCAddress,
-output wire[23:0] Instruction);
+output wire[23:0] Instruction
+);
 
 reg[7:0] instrMem[63:0];
 
 initial
 $readmemb("instructionMemory.mem", instrMem);
 
-assign Instruction[23:16] = instrMem[PCAddress];
-assign Instruction[15:8] = instrMem[PCAddress + 32'd1];
-assign Instruction[7:0] = instrMem[PCAddress + 32'd2];
+assign Instruction[23:20] = instrMem[PCAddress];
+assign Instruction[19:16] =instrMem[PCAddress+ 24'd0]
+assign Instruction[15:12] = instrMem[PCAddress + 24'd1];
+assign Instruction[11:8] = instrMem[PCAddress + 24'd2];
+assign Instruction[7:4] = instrMem[PCAddress + 24'd3];
+assign Instruction[3:0]= instrMem[PCAddress+ 24'd4]
 
 
 endmodule
