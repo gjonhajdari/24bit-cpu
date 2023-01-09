bd017317e5e24ae359103d9fa29a703012f16926
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-01-09 01:55:56 +0100

    Update and rename MUX4to1.v to mux4ne1.v

--- src/cpu/mux4ne1.v
+++ src/cpu/mux4ne1.v
@@ -19,16 +19,19 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module Mux4to1(
-    input Hyrja0,
-    input Hyrja1,
-    input Hyrja2,
-    input Hyrja3,
-    input [1:0] S,
-    output Dalja
-    );
+module mux4ne1(
+input Hyrja0,
+input Hyrja1,
+input Hyrja2,
+input Hyrja3,
+input [1:0] S,
+output Dalja
+);
+// S 00 H0
+// S 01 H1
+//S 10  H2
+//S 11  H3
     
-    assign Dalja = S[1] ? (S[0] ? Hyrja3 : Hyrja2) : (S[0] ? Hyrja1 : Hyrja0);
+assign Dalja = S[1] ? (S[0] ? Hyrja3 : Hyrja2) : (S[0] ? Hyrja1 : Hyrja0);
     
 endmodule
