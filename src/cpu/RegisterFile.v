f5b019af686137e58d78ec424dea234b3bb5e4b4
Gentrit Bytyçi <97307142+Genti1bytyqi@users.noreply.github.com>
2023-01-09 19:33:10 +0100

    Update RegisterFile

--- src/cpu/RegisterFile.v
+++ src/cpu/RegisterFile.v
@@ -19,36 +19,28 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module RegisterFile(
-input wire[4:0] RS,
-input wire[4:0] RT,
-input wire[4:0] RD,
-input wire[23:0] WriteData,
-input wire RegWrite,
-input wire Clock,
-output wire[23:0] ReadRS,
-output wire[23:0] ReadRT
+input wire[3:0] RS,
+input wire[3:0] RT,
+input wire[3:0] RD,
+input wire [23:0] WriteData,
+output [23:0] ReadRS,
+output [23:0] ReadRT,
+input RegWrite,
+input Clock
     );
+    reg[23:0] Register[23:0];
+    always @ (posedge Clock)
+    begin
+    if(RegWrite)
+    begin
+     Register[RD] <= WriteData;
+     end
+        
+     end
     
-reg[23:0] Registers[23:0];
+      assign ReadRS = Register[RS]; 
+      assign ReadRT = Register[RT]; 
 
-//Reseto te gjithe regjistrat ne 0
-integer i;
-initial 
-begin
-for(i=0;i<24;i=i+1)
-   Registers[i] <= 24'd0; 
-end
 
-//Shkruaj ne regjiter
-always @(posedge Clock)
-begin
-Registers[RD] <= WriteData;
-end
-
-//lexo regjistrat ReadData1, ReadData2
-assign ReadRS = Registers[RS];
-assign ReadRT = Registers[RT];
-
-endmodule
+endmodule   
