788ee65fbcf38fcb259e829e0722d07df28a78b2
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-01-09 12:46:37 +0100

    changed it to 24 bit

--- src/cpu/InstructionMemory.v
+++ src/cpu/InstructionMemory.v
@@ -21,18 +21,17 @@
 
 
 module InstructionMemory(
-input wire[31:0] PCAddress,
-output wire[31:0] Instruction);
+input wire[23:0] PCAddress,
+output wire[23:0] Instruction);
 
 reg[7:0] instrMem[63:0];
 
 initial
 $readmemb("instructionMemory.mem", instrMem);
 
-assign Instruction[31:24] = instrMem[PCAddress];
-assign Instruction[23:16] = instrMem[PCAddress + 32'd1];
-assign Instruction[15:8] = instrMem[PCAddress + 32'd2];
-assign Instruction[7:0] = instrMem[PCAddress + 32'd3];
+assign Instruction[23:16] = instrMem[PCAddress];
+assign Instruction[15:8] = instrMem[PCAddress + 32'd1];
+assign Instruction[7:0] = instrMem[PCAddress + 32'd2];
 
 
 endmodule
