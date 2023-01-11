2446d70c2128faa91b2d231d86596e08c923cad5
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-01-11 23:10:44 +0100

    Updated multiplexer (again)

--- src/cpu/MUX_4to1.v
+++ src/cpu/MUX_4to1.v
@@ -28,26 +28,13 @@ module MUX_4to1 (
 	output OUT
 );
 
-reg mOUT;
 
-// S 00 H0
-// S 01 H1
-// S 10 H2
-// S 11 H3
+// S 000 H0
+// S 001 H1
+// S 010 H2
+// S 011 H3
 
-always @(Selector or input0 or input1 or input2 or input3)
-begin
-	case(Selector)
-	// Add other inputs later if needed
-		3'b000: mOUT = input0;
-		3'b001: mOUT = input1;
-		3'b010: mOUT = input2;
-		3'b011: mOUT = input3;
-	endcase
-
-	OUT = mOUT;
-end
     
-// assign Dalja = S[1] ? (S[0] ? Hyrja3 : Hyrja2) : (S[0] ? Hyrja1 : Hyrja0);
+assign OUT = S[1] ? (S[0] ? Hyrja3 : Hyrja2) : (S[0] ? Hyrja1 : Hyrja0);
     
 endmodule
