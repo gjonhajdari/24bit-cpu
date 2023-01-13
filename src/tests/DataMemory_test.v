344ec73e3e8cc9f7cc7ad6ede04d050e2f611916
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-01-13 22:38:12 +0100

    Update DataMemory_test.v

--- src/tests/DataMemory_test.v
+++ src/tests/DataMemory_test.v
@@ -8,12 +8,12 @@ module DataMemory_test();
 
 initial 
 begin 
-	#0 Clock =1'b0;
-	#5 MemWrite = 1'b1; Address = 24'd2; WriteData=24'd7; MemRead=1'b0;
-	#5 Clock=1'b1;
-	#5 Clock=1'b0; MemWrite=1'b0;
-	#5 MemRead=1'b1; Address=24'd2;
-	#5 $stop;
+#0 Clock =1'b0;
+#5 MemWrite = 1'b1; Address = 24'd2; WriteData=24'd7; MemRead=1'b0;
+#5 Clock=1'b1;
+#5 Clock=1'b0; MemWrite=1'b0;
+#5 MemRead=1'b1; Address=24'd2;
+#5 $stop;
 end
 
 DataMemory DM(Address, WriteData, Clock, MemWrite, MemRead, ReadData);
